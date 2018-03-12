68a2db450aa360706e85a02b71b30d2edfab0c81
harrisonmk <harrison.mitchell@hotmail.com>
2018-03-12 16:55:14 -0400

    Completo

--- src/lpootrabalho/LpooIntroducao.java
+++ src/lpootrabalho/LpooIntroducao.java
@@ -44,15 +44,49 @@ public class LpooIntroducao {
        contas[1]=conta1;
        contas[2] =conta2;
        
-             
-       
+                    
        p1.setConta(contas);
        
        System.out.println(p1.obterInfor());
        
+       Pessoa p2 = new Pessoa();
        
+       p2.setNome("Ana");
+       p2.setDate("20/05/1995");
+       p2.setSexo('F');
+       p2.setCpf("740.051.460-60");
+       
+       
+       Conta c = new Conta();
+       
+       c.setSaldo(10.20);
+       c.setSenha(8080);
+       c.setNumero(98765);
+       c.setAgencia(23);
+
+       Conta c1 = new Conta();
+       
+       c1.setSaldo(3.56);
+       c1.setSenha(1414);
+       c1.setNumero(843334);
+       c1.setAgencia(22);
+       
+       Conta c2 = new Conta();
+       
+       c2.setSaldo(5.70);
+       c2.setSenha(2445);
+       c2.setNumero(987654);
+       c2.setAgencia(12);
+        
+       
+       Conta[] contas2 = new Conta[3];
+       contas2[0]= c;
+       contas2[1]=c1;
+       contas2[2] =c2;
     
-               
+      p2.setConta(contas2);
+       
+       System.out.println(p2.obterInfor());         
         
     }
 
